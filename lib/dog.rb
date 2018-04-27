ec3433b163ee181348d4c85cb80dca695a456f22
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-04-27 17:37:07 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,8 @@ class Dog
 
   def initialize(name)
     @name = name
-    @@all << self #they wanted the array to be filled with Dog objects
+    @@all << self #they wanted the array to be filled with Dog objects hence the
+    #expected ids in the test
   end
 
   def self.clear_all
@@ -14,7 +15,7 @@ class Dog
 
   def self.all
     @@all.each do |doggie|
-      puts doggie.name #they wanted to have the
+      puts doggie.name #they wanted to have the only the names in the test for this.
     end
   end
 end
